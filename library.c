a321d772ca09a7f2d7366278fc840dc7631feaf7
Speedy Turtle <danieljustice09@gmail.com>
2017-09-30 17:28:53 -0400

    it clearsgit add .git add .

--- library.c
+++ library.c
@@ -1,7 +1,15 @@
 #include <fcntl.h>          /* for open() syscall ref:http://pubs.opengroup.org/onlinepubs/009695399/functions/open.html*/
 #include <stropts.h>        /* for ioctl() syscall ref: http://pubs.opengroup.org/onlinepubs/009695399/functions/ioctl.html*/
 #include <sys/mman.h>       /* for mmap() syscall ref:http://pubs.opengroup.org/onlinepubs/009695399/functions/mmap.html*/
-#include <linux/fb.h>       /* good example  of ioctl http://www.ummon.eu/Linux/API/Devices/framebuffer.html*/
+#include <linux/fb.h>       /* good example of ioctl http://www.ummon.eu/Linux/API/Devices/framebuffer.html*/
+#include <unistd.h>         /* for write() http://man7.org/linux/man-pages/man2/write.2.html*/
+
+
+
+
+//function prototypes
+void clear_screen();
+
 
 //for mmap in init
 int file;
@@ -13,6 +21,8 @@ struct fb_var_screeninfo variable_info;
 struct fb_fix_screeninfo fixed_info;
 int yres;
 int line_length;
+
+#define CLEAR_CODE "\033[2J" 
 
 
 void init_graphics(){
@@ -26,7 +36,11 @@ void init_graphics(){
     len = variable_info.yres_virtual * fixed_info.line_length;
     address = mmap(0, len, PROT_WRITE, MAP_PRIVATE, file, 0);
     //clears the screen
-    print("\033[2J");
+    clear_screen();
     
 
+}
+
+void clear_screen(){
+    write(1, CLEAR_CODE, sizeof CLEAR_CODE);        //https://en.wikipedia.org/wiki/Write_(system_call)
 }
